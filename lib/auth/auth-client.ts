b7d62779d6ded9bd408635a335a4a109c26053d8
Srinikesh L <srinikesh003@gmail.com>
2026-09-13 00:32:24 +0530

    fix signup

--- lib/auth/auth-client.ts
+++ lib/auth/auth-client.ts
@@ -1,17 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
-const getBaseUrl = () => {
-  // 1. In the browser, always use the active domain dynamically
-  if (typeof window !== "undefined") {
-    return window.location.origin;
-  }
-  
-  // 2. Fallback for the Next.js compiler during the build phase
-  return "http://localhost:3000";
-};
-
 export const authClient = createAuthClient({
-  baseURL: getBaseUrl()
+  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL!,
 });
 
 export const { signIn, signUp, signOut, useSession } = authClient;
